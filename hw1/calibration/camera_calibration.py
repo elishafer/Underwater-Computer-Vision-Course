2ec90c9b912a0497035fc05d9971920bfab262f0
elishafer <16764947+elishafer@users.noreply.github.com>
2018-11-18 19:20:20 +0200

    Initial commit. HW1 part 1 complete.

---
+++ hw1/calibration/camera_calibration.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+import glob
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# Size of Checkerboard
+checkerboard_size = (9,6)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)*30.5
+objp = objp.reshape(-1,1,3)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('./checkerboard/*.jpg')
+
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, checkerboard_size,None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, checkerboard_size, corners2,ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(500)
+        
+cv2.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+
+
---
+++ hw1/calibration/project_to_2d.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2
+
+
+
+def project_to_2d(coordinates_3d):
+    """
+
+    :param coordinates_3d: 3d coordinates of point. tuple (x,y,z)
+    :return: 2d coordinates in 2d camera projection.
+    """
+    calibration_matrix = [[2560.5518418869233, 0.0, 1533.530354490828],
+                          [0.0, 2560.2351813290616, 1206.2615781781851],
+                          [0.0, 0.0, 1.0]]
+
+    calibration_matrix = np.matrix(calibration_matrix)
+
+
+    coordinates_3d = np.array(coordinates_3d)
+    coordinates_3d.shape = (3,1)
+    # coordinates_3d.transpose()
+    # np.swapaxes(coordinates_3d,0,1)
+    p_2d = calibration_matrix * coordinates_3d
+    p_2d = p_2d / coordinates_3d[2]
+
+    return p_2d
+
+if __name__ == '__main__':
+    coord_1 = (1,1,1)
+    coord_2 = (1,1,10)
+
+    print('2d projection of coordinate 1 %:\n',coord_1)
+    coord_2d = project_to_2d(coord_1)
+    print coord_2d
+
+    print('2d projection of coordinate 2 %:\n', coord_2)
+    coord_2d = project_to_2d(coord_2)
+    print coord_2d
